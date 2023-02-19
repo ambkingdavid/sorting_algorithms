38c25da639b1566515460419e968886c66b2fd83
kelvin <kelvinwooldbridge@gmail.com>
2023-02-19 13:31:11 +0100

    add fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,23 +9,38 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	int temp,  *min;
-	size_t i, j;
+	int temp,  index, *min;
+	int i, j;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = array + i;
+		/*min = array + i;*/
+		index = i;
 		for (j = i + 1; j < size; j++)
-			min = (array[j] < *min) ? (array + j) : min;
+		{
+			if (array[j] < array[index])
+				index = j;
+			else
+				index = index;
+			/*min = (array[j] < *min) ? (array + j) : min;*/
+		}
 
-		if ((array + i) != min)
+		/*if ((array + i) != min)
 		{
 			temp = *(array + i);
 			*(array + i) = *min;
 			*min = temp;
+			print_array(array, size);
+		}
+		*/
+		if (i != index)
+		{
+			temp = array[i];
+			array[i] = array[index];
+			array[index] = temp;
 			print_array(array, size);
 		}
 	}
