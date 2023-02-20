d956510a53c88eb5e30ed2ce2a5ab433d4524451
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-02-20 18:14:01 +0100

    add task 3

--- sort.h
+++ sort.h
@@ -23,6 +23,10 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+int partition(int *array, int min_idx, int max_idx, size_t size);
+void lomuto_partition(int *array, int min_idx, int max_idx, size_t size);
+void quick_sort(int *array, size_t size);
+
 
 
 #endif /*SORT_H*/
